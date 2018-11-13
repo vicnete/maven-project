63cce7dbde2e614077704f2a9d71bdbdd95b1a14
vicnete <vicnete@gmail.com>
2018-11-13 10:19:14 +0100

    Update server/src/main/java/com/example/Greeter.java
    Probando de nuevo

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,16 +1,4 @@
 package com.example;
-
-
-/**
- * Domain classes used to produce the JSON and XML output for the RESTful services. 
- * <p>
- * These classes contain the JAXB annotations.
- *
- * @since 1.0
- * @author jwhite
- * @version 1.1
- */
-
 public class Greeter {
 
   /**
@@ -19,7 +7,7 @@ public class Greeter {
   public Greeter() {
 
   }
-  public String greet( final String someone) {
+  public final String greet( final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
